5f787d2b4dfcd911386437805308c8b8e358a8c3
cjkoziol <70605379+cjkoziol@users.noreply.github.com>
2020-11-12 17:51:05 -0500

    added comments toward skeleton

--- Plague_Gamez.py
+++ Plague_Gamez.py
@@ -7,3 +7,7 @@ import requests
 #create database
 print(requests.get('https://api.covid19api.com/').text)
 # curl --location --request GET 'https://api.covid19api.com/'
+
+
+#COVID API = use GET Day One
+#Steam API = use GetRecentlyPlayedGames (v0001) http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=76561197960434622&format=json
