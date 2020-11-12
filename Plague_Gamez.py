4bd375fd9640c02ed46dd522ff10192ed0b2e28a
cjkoziol <70605379+cjkoziol@users.noreply.github.com>
2020-11-12 17:50:11 -0500

    add skeleton

--- Plague_Gamez.py
+++ Plague_Gamez.py
@@ -2,5 +2,8 @@ import sqlite3
 import json
 import os
 import matplotlib
+import requests
 
 #create database
+print(requests.get('https://api.covid19api.com/').text)
+# curl --location --request GET 'https://api.covid19api.com/'
