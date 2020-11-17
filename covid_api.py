f5c090355ab59f9af0f717b53d7d4b5701cc1fc3
Aman Khalid <aakhalid@umich.edu>
2020-11-17 18:22:39 -0500

    Added get_US_confirmed_cases(); gets US cases for given time frame

--- covid_api.py
+++ covid_api.py
@@ -1,19 +1,21 @@
 import requests
 
-SUMMARIES_URL = "https://api.covid19api.com/summary"
+US_CASES_BASE_URL = "https://api.covid19api.com/total/country/united-states/status/confirmed"
 
-def get_US_summary():
+def get_US_confirmed_cases(from_date, to_date):
+    url = f"{US_CASES_BASE_URL}?from={from_date}T00:00:00Z&to={to_date}T00:00:00Z"
     try:
-        summary_request = requests.get(SUMMARIES_URL)
-        if not summary_request:
-            raise Exception(summary_request.status_code)
+        cases_request = requests.get(url)
+        if not cases_request:
+            raise Exception(cases_request.status_code)
 
-        summary_json = summary_request.json()
-        for summary in summary_json["Countries"]:
-            if summary["CountryCode"] == "US":
-                return summary
-        
-        raise Exception("US summary not returned.")
+        return cases_request.json()
     except Exception as e:
-        print("An exception occurred Here's the message:")
+        print("An exception occurred. Here's the message:")
         print(e)
+
+def main():
+    print(get_US_confirmed_cases("2020-11-01", "2020-11-02"))
+
+if __name__ == "__main__":
+    main()
