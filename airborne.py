5dbbfbcd10136bb41eef12749d632982f0dae7c7
Aman Khalid <aakhalid@umich.edu>
2020-11-25 19:11:40 -0500

    Added GUI for state/month input

--- airborne.py
+++ airborne.py
@@ -1,13 +1,63 @@
-import sqlite3
-import json
-import os
 import matplotlib
 import requests
+import tkinter as tk
+from tkinter import font
+from tkinter import ttk
 
-#create database
-print(requests.get('https://api.covid19api.com/').text)
-# curl --location --request GET 'https://api.covid19api.com/'
+MONTHS = (
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+)
 
+def get_locations():
+    with open("locations.txt", "r") as locations_file:
+        states_list = []
+        cities_list = []
+        for line in locations_file:
+            states_list.append(line.split(",")[0])
+            cities_list.append(line.split(",")[1])
+        return states_list, cities_list
 
-#COVID API = use GET Day One
-#Steam API = use GetRecentlyPlayedGames (v0001) http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=76561197960434622&format=json
+def selection_GUI(state_list):
+    root = tk.Tk()
+    root.geometry("250x250")
+    root.eval("tk::PlaceWindow . center")
+    root.title("Airborne")
+    welcome_font = font.Font(family="Courier", size=14)
+    normal_font = font.Font(family="Roboto", size=12)
+    small_font = font.Font(family="Roboto Condensed", size=10)
+    welcome_label = tk.Label(root, text="Welcome to Airborne!", font=welcome_font).pack(pady=10)
+
+    # State selection
+    state_label = tk.Label(root, text="Please select a state:", font=normal_font).pack()
+    state = tk.StringVar()
+    state.set(state_list[0])
+    state_menu = ttk.Combobox(root, width=15, textvariable=state,
+                              values=state_list, font=small_font).pack()
+    
+    spacing = tk.Label(root).pack()
+    
+    # Month selection
+    month_label = tk.Label(root, text="Please select a month:", font=normal_font).pack()
+    month = tk.StringVar()
+    month.set(MONTHS[0])
+    month_menu = ttk.Combobox(root, width=15, textvariable=month, values=MONTHS, font=small_font).pack()
+
+    continue_button = tk.Button(root, text="Continue", font=normal_font, command=root.quit).pack(pady=20)
+
+    root.mainloop()
+    return state.get(), month.get()
+
+def main():
+    states, cities = get_locations()
+    selected_state, selected_month = selection_GUI(states)
+
+if __name__ == "__main__":
+    main()
--- covid_api.py
+++ covid_api.py
@@ -7,22 +7,20 @@ def get_cases_per_month(state, month):
 
 def get_daily_cases(state, date):
     url = f"{BASE_URL}{state}/{date}.json"
-    print(url)
     try:
         cases_request = requests.get(url)
         if not cases_request:
-            raise Exception(cases_request.status_code)
+            raise Exception()
 
         return cases_request.json()
     except Exception as e:
-        print("An exception occurred. Here's the message:")
-        print(e)
+        return None
 
 def add_to_COVID_database(cur, conn):
     pass
 
-# def main():
-#     print(get_daily_cases("ca", "20200501"))
+def main():
+    print(get_daily_cases("ca", "20200301"))
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
