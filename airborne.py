bf9f59270d874cb24cfc0d4d390966f5a3628173
Aman Khalid <aakhalid@umich.edu>
2020-11-27 20:38:51 -0500

    Set up databases; covid_api works!

--- airborne.py
+++ airborne.py
@@ -1,8 +1,12 @@
 import matplotlib
 import requests
+import sqlite3
+import sys
 import tkinter as tk
 from tkinter import font
 from tkinter import ttk
+
+import covid_api
 
 MONTHS = (
     "April",
@@ -12,8 +16,7 @@ MONTHS = (
     "August",
     "September",
     "October",
-    "November",
-    "December"
+    "November"
 )
 
 def get_locations():
@@ -26,8 +29,8 @@ def get_locations():
 
 def selection_GUI(state_list):
     root = tk.Tk()
-    root.geometry("250x250")
     root.eval("tk::PlaceWindow . center")
+    root.protocol("WM_DELETE_WINDOW", sys.exit)
     root.title("Airborne")
     welcome_font = font.Font(family="Courier", size=14)
     normal_font = font.Font(family="Roboto", size=12)
@@ -49,14 +52,112 @@ def selection_GUI(state_list):
     month.set(MONTHS[0])
     month_menu = ttk.Combobox(root, width=15, textvariable=month, values=MONTHS, font=small_font).pack()
 
-    continue_button = tk.Button(root, text="Continue", font=normal_font, command=root.quit).pack(pady=20)
+    continue_button = tk.Button(root, text="Continue", font=normal_font, command=root.destroy).pack(pady=20)
+    warning_msg = "Note: Downloading the data will take time.\nDon't be alarmed if you don't see anything for a few seconds!"
+    warning_label = tk.Label(root, text=warning_msg, font=small_font).pack(padx=15, pady=10)
 
     root.mainloop()
     return state.get(), month.get()
 
-def main():
+def display_error_message(status_code, COVID_API=False):
+    root = tk.Tk()
+    root.eval("tk::PlaceWindow . center")
+    root.protocol("WM_DELETE_WINDOW", sys.exit)
+    root.title("Airborne")
+    normal_font = font.Font(family="Roboto", size=12)
+    error_font = font.Font(family="Courier", size=12)
+    header_label = tk.Label(root, text="Oops...", font=normal_font).pack(pady=10)
+
+    if COVID_API:
+        error_API = "COVID-19"
+    else:
+        error_API = "OpenAQ"
+    
+    error_label = tk.Label(root, text=f"An error occurred while collecting the {error_API} data. Here's the server's response:", font=normal_font).pack(padx=15)
+    response_abel = tk.Label(root, text=status_code, font=error_font).pack()
+
+    exit_button = tk.Button(root, text="Exit", font=normal_font, command=root.destroy).pack(pady=20)
+    root.mainloop()
+
+def display_load_finished_message():
+    root = tk.Tk()
+    root.eval("tk::PlaceWindow . center")
+    root.protocol("WM_DELETE_WINDOW", sys.exit)
+    root.title("Airborne")
+    normal_font = font.Font(family="Roboto", size=12)
+    msg_label = tk.Label(root, text="Data finished downloading!", font=normal_font).pack(padx=15, pady=10)
+    continue_button = tk.Button(root, text="Continue", font=normal_font, command=root.destroy).pack(pady=20)
+
+    root.mainloop()
+
+def check_if_table_exists(cur, table_name):
+    cur.execute(f"SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+    return cur.fetchone()[0] == 1
+
+def set_up_locations_table(cur):
+    if check_if_table_exists(cur, "Locations"):
+        return
+    
+    cur.execute("CREATE TABLE Locations (id INTEGER PRIMARY KEY, state TEXT, city TEXT)")
     locations = get_locations()
-    selected_state, selected_month = selection_GUI(list(locations.keys()))
+    count = 1
+    for state in locations:
+        cur.execute("INSERT INTO Locations (id, state, city) VALUES (?, ?, ?)", (count, state, locations[state],))
+        count += 1
+
+def set_up_months_table(cur):
+    if check_if_table_exists(cur, "Months"):
+        return
+    
+    cur.execute("CREATE TABLE Months (id INTEGER PRIMARY KEY, month TEXT)")
+    count = 4
+    for month in MONTHS:
+        cur.execute("INSERT INTO Months (id, month) VALUES (?, ?)", (count, month,))
+        count += 1
+
+def set_up_tables(cur, conn):
+    set_up_locations_table(cur)
+    set_up_months_table(cur)
+    
+    cur.execute("""CREATE TABLE IF NOT EXISTS COVID_Cases
+                   (date INTEGER PRIMARY KEY, month_id INTEGER, location_id INTEGER, new_cases INTEGER,
+                   FOREIGN KEY (month_id) REFERENCES Months (id),
+                   FOREIGN KEY (location_id) REFERENCES Locations (id),
+                   UNIQUE(date, location_id))""")
+    cur.execute("""CREATE TABLE IF NOT EXISTS Air_Quality
+                   (date INTEGER PRIMARY KEY, month_id INTEGER, location_id INTEGER, quality REAL,
+                   FOREIGN KEY (month_id) REFERENCES Months (id),
+                   FOREIGN KEY (location_id) REFERENCES Locations (id),
+                   UNIQUE(date, location_id))""")
+    conn.commit()
+
+def main():
+    # Connect to database
+    conn = sqlite3.connect("airborne_database.db")
+    cur = conn.cursor()
+    set_up_tables(cur, conn)
+    locations = get_locations()
+
+    state, month = selection_GUI(list(locations.keys()))
+    cur.execute("SELECT * FROM Locations WHERE state = ?", (state,))
+    location = cur.fetchone()
+    cur.execute("SELECT id FROM Months WHERE month = ?", (month,))
+    month_num = cur.fetchone()[0]
+    month = str(month_num)
+    if month_num < 10:
+        month = "0" + month
+
+    COVID_status = covid_api.API_driver(location[1].lower(), location[0], month, cur, conn)
+    if not COVID_status[0]:
+        display_error_message(COVID_status[1], COVID_API=True)
+        return
+    
+    # openAQ_status = air_quality_api.API_driver(location[2], month, cur, conn)
+    # if not openAQ_status[0]:
+    #     display_error_message(openAQ_status[1])
+    #     return
+
+    display_load_finished_message()
 
 if __name__ == "__main__":
     main()
--- covid_api.py
+++ covid_api.py
@@ -2,25 +2,45 @@ import requests
 
 BASE_URL = "https://api.covidtracking.com/v1/states/"
 
-def get_cases_per_month(state, month):
-    pass
-
 def get_daily_cases(state, date):
     url = f"{BASE_URL}{state}/{date}.json"
     try:
         cases_request = requests.get(url)
-        if not cases_request:
-            raise Exception()
-
-        return cases_request.json()
-    except Exception as e:
+    except:
         return None
+    
+    return cases_request
 
-def add_to_COVID_database(cur, conn):
-    pass
+def add_to_COVID_database(json_data, date, location_id, cur, conn):
+    month = int(date[4:6])
+    date = int(date)
+    new_cases = json_data["positiveIncrease"]
+    cur.execute("INSERT INTO COVID_Cases (date, month_id, location_id, new_cases) VALUES (?, ?, ?, ?)",
+                (date, month, location_id, new_cases))
+    conn.commit()
 
-def main():
-    print(get_daily_cases("ca", "20200301"))
+def API_driver(state, location_id, month, cur, conn):
+    for i in range(1, 26):
+        if i < 10:
+            date = f"2020{month}0{i}"
+        else:
+            date = f"2020{month}{i}"
+        
+        daily_cases_request = get_daily_cases(state, date)
+        if daily_cases_request is None:
+            return (False, "No status code received")
+        if not daily_cases_request:
+            return (False, daily_cases_request.status_code)
+        
+        try:
+            add_to_COVID_database(daily_cases_request.json(), date, location_id, cur, conn)
+        except:
+            break
+    
+    return (True,) 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     COVID_api_driver("al", "04", None, None)
+
+# if __name__ == "__main__":
+#     main()
